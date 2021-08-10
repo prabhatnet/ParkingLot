28cb8f0a9c26a788d96a26c5a3fd28ac8dba28ee
leoprabz <leoprabz@gmail.com>
2021-08-10 20:56:37 +0530

    Adding the Person Class

---
+++ BL/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot.BL
+{
+    class Person
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public string VehicleNumber { get; set; }
+    }
+}
