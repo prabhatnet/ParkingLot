b5e1c3dfcbc5da04d5ddcbfc72627dbe58f25182
Prabhat Sinha <prabhatsinha@gmail.com>
2021-08-11 20:15:12 +0530

    Added Car Number

--- BL/Entities.cs
+++ BL/Entities.cs
@@ -13,6 +13,7 @@ namespace ParkingLot.BL
         {
         public int id { get; set; }
         public string Name { get; set; }
+        public string CarNumber { get; set; }
            
         }
         public class System
